24d7ea57dbe8a0a65ecea4e53ee70b35f5067b42
Volodymyr Dedenko <dedenkof@ukr.net>
2025-09-12 13:15:53 +0300

    removing redundant markup and adding :host to styles, examples with @HostBinding and @HostListener

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,20 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
 	encapsulation: ViewEncapsulation.None,
 	host: {
-		class: "control"
+		class: 'control',
+		'(click)': 'onClick()'
 	}
 })
 export class ControlComponent {
+	// @HostListener('click') onClick() {
+	// 	console.log('Click', 'knock');
+	// }
+
 	label = input.required();
+//	@HostBinding('class') className ='control';
+
+onClick() {
+ 	console.log('Click', 'knock');
+ }
 
 }
