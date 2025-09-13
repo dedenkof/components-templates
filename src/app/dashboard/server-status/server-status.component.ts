09dfb9e232bc19bdebbea364c8b60c7e2126eb08
Volodymyr Dedenko <dedenkof@ukr.net>
2025-09-13 11:28:04 +0300

    ngOnInit and constuctor

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -6,10 +6,25 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
-	host: {
-		id: 'status'
-	}
+  host: {
+    class: 'status',
+  },
 })
 export class ServerStatusComponent {
-	currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
