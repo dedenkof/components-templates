b905c9421c23b7652e080b6db4ed59ec9681e1cb
Volodymyr Dedenko <dedenkof@ukr.net>
2025-09-15 16:30:57 +0300

    add AfterViewInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -10,14 +10,19 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
-	// @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-	private form = viewChild<ElementRef<HTMLFormElement>>('form');
+export class NewTicketComponent implements AfterViewInit{
+	@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+	// private form = viewChild<ElementRef<HTMLFormElement>>('form');
+
+	ngAfterViewInit() {
+		console.log('After Vie Init');
+		console.log(this.form?.nativeElement);
+	}
 
 	onSubmit(title: string, ticketText: string) {
 	console.log(title);
 	console.log(ticketText);
-	this.form()?.nativeElement.reset();
+	this.form?.nativeElement.reset();
 }
 
 }
